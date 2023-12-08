86eb9df4313f03194eba58dfbb287370dc4a0823
SourVoice <2985122798@qq.com>
2023-12-08 20:21:32 +0800

    添加第一个测试

---
+++ Test/include/algorithm_test.h
@@ -0,0 +1,40 @@
+#ifndef MY_ALGORITHM_TEST_H
+#define MY_ALGORITHM_TEST_H
+
+// 标准
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <functional>
+
+// 目标
+#include "../src/algorithm.h"
+
+#include "test.h"
+
+namespace MySTL {
+
+namespace test {
+
+namespace algorithm_test {
+
+TEST(copy_test) {
+    int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int exp[5], act[5];
+
+    std::copy(arr1, arr1 + 5, exp);
+    MySTL::copy(arr1, arr1 + 5, act);
+    EXPECT_CON_EQ(exp, act);
+
+    std::copy(arr1 + 5, arr1 + 10, exp);
+    MySTL::copy(arr1 + 5, arr1 + 10, act);
+    EXPECT_CON_EQ(exp, act);
+}
+
+} // namespace algorithm_test
+
+}  // namespace MySTL::test
+
+}  // namespace MySTL::test::algorithm_test
+
+#endif
---
+++ Test/test.cpp
@@ -0,0 +1,11 @@
+
+#include "test.h"
+#include "algorithm_test.h"
+
+int main() {
+    using namespace MySTL::test;
+    std::cout.sync_with_stdio(false);
+
+    RUN_ALL_TESTS();
+
+}
